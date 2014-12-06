c6e83fad4a8e07f6cd727c2b541508f9ce21f9b8
Ryan Erdmann <ryanerdmann@hotmail.com>
2014-12-05 19:51:38 -0600

    Code from Deliverable 1

--- server.R
+++ server.R
@@ -1,23 +1,53 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(Quandl)
+
+# Returns a vector of selected symbols
+getSelectedSymbols <- function(input) {
+  symbols <- sapply(strsplit(input$symbols, ","), function(str) {gsub("(^ +)|( +$)", "", str)}, simplify=FALSE)[[1]]
+  #symbols <- strsplit(input$symbols, "\ |,")[[1]]
+  symbols
+}
+
+getDates <- function(input) {
+  input$dates
+}
+
+# Pulls stock history from Quandl using the Yahoo dataset.
+# History is sorted and parsed into a data.frame with Date and Closing price
+get.stock.history <- function(symbol, start, end) {
+  allData <- Quandl(paste0("YAHOO/", symbol), trim_start=start, trim_end=end, sort_order="asc", authcode="DtnoHM-ZkzznSdTzty8L", collapse="weekly")
+  filteredData <- data.frame(
+    Date = allData[,'Date'],
+    Close = allData[,'Adjusted Close'])  
+  filteredData
+}
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  output$selectedPortfolios <- renderText({
+    getSelectedSymbols(input)
   })
-
+  
+  output$timeseriesPlot <- renderPlot({
+    symbols <- getSelectedSymbols(input)
+    dates <- getDates(input)
+    colors <- c('Black', 'Blue', 'Green', 'Red')
+    
+    histories <- lapply(symbols, get.stock.history, dates[1], dates[2])
+    max.price <- max(sapply(histories, function(history) { max(history$Close) }))
+    
+    
+    stock.history <- histories[[1]]
+    plot(stock.history, type="l", main="Weekly Time Series", col=colors, ylim=c(0, max.price))
+    
+    if (length(symbols) > 1) {
+      for (i in seq(2, length(symbols))) {
+        stock.history <- histories[[i]]
+        lines(stock.history, col=colors[i], type="l")
+      }
+    }
+    
+    legend('bottomright', symbols, lwd=2, col=colors)
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,24 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
+  
+  titlePanel("Project 281H"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      p("Financial Analysis over a Timer Period using the R"),
+      textInput("symbols", label = h6("Symbol(s)"), value = "XOM, IBM, AAL"),
+      checkboxGroupInput("selectedAnalysis", label = h6("Analysis"), 
+                         choices = list("Fundamental Analysis" = 1, "Time Series Analysis" = 2, "Portfolio Analysis" = 3),
+                         selected = c(2)),
+      dateRangeInput("dates", label = p("Date range: yyyy-mm-dd"), format="yyyy-mm-dd", start="2013-11-24", end="2014-11-24"),
+      submitButton(text = "Submit")
     ),
-
-    # Show a plot of the generated distribution
+    
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("timeseriesPlot"),
+      p("For this milestone of the application, we have implemented the simple time series analysis.  You can plot multiple stock symbols on the chart over a custom date range.  Currently, we only support weekly data.  Future iterations of this application will support more granular stock data configuration and additional analysis types."),
+      textOutput("selectedPortfolios")
     )
   )
 ))
