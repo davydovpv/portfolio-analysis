7049ac1903fcca93c9846bcaf2d7e20f745c3681
Ryan Erdmann <ryanerdmann@hotmail.com>
2014-12-07 19:20:33 -0600

    Hook up custom HTML/CSS/JS UI

--- inputParsing.R
+++ inputParsing.R
@@ -1,8 +1,14 @@
 # Returns a vector of selected symbols
 # input$symbols is a comma separated string, potentially with spaces
 getSelectedSymbols <- function(input) {
-  symbols <- sapply(strsplit(input$symbols, ","), function(str) {gsub("(^ +)|( +$)", "", str)}, simplify=FALSE)[[1]]
+  symbols <- strsplit(input$selectedSymbols, ",")
   symbols
+}
+
+getSelectedWeights <- function(input) {
+  weightstrs <- strsplit(input$selectedWeights, ",")
+  weights <- c(do.call("cbind",lapply(weightstrs, as.numeric))) 
+  weights
 }
 
 getStartDate <- function(input) {
--- server.R
+++ server.R
@@ -9,6 +9,10 @@ source('functions.R')
 
 shinyServer(function(input, output, session) {
   
+  output$debug <- renderText({
+    getEndDate(input)
+  })
+  
   currentPrices <- reactive({
     symbols <- getSelectedSymbols(input)
     start <- getStartDate(input)
@@ -26,84 +30,10 @@ shinyServer(function(input, output, session) {
     returns <- get.closing.returns(symbols, start, end)
     returns
   })
-   
+  
   # Timeseries Page --------------- 
   
   
   
-  # Statistics Page ---------------
   
-  output$statisticsStockMarketTable <- renderTable({
-    marketTable <- data.frame(
-      erp = c("6.34%"),
-      ret = c("8.3%"),
-      sig = c("3.4%"))
-    
-    colnames(marketTable) <- c("ERP", "Return", "Standard Deviation")
-    
-    marketTable
-  })
-  
-  output$statisticsStocksTable <- renderTable({
-    stocksTable <- data.frame(
-      Symbol = c('MSFT', 'AAPL'),
-      Industry = c('Software', 'Software'),
-      Beta = c(0,0),
-      AnnualReturn = c(0,0),
-      ExpectedReturn = c(0,0),
-      StandardDeviation = c(0,0),
-      High = c(0,0),
-      Low = c(0,0),
-      Average = c(0,0),
-      Profit = c(0,0),
-      SharpeRatio = c(0,0)
-      )
-    
-    stocksTable    
-  })
-  
-  output$statisticsPortfolioTable <- renderTable({
-    portfolioTable <- data.frame(
-      AnnualReturn = c(0),
-      ExpectedReturn = c(0),
-      StandardDeviation = c(0)
-    )
-    
-    portfolioTable    
-  })
-  
-  output$statisticsPortfolioCorrelation <- renderText({
-    cor(currentReturns())
-  })
-  
-  output$statisticsPortfolioCovariance <- renderText({
-    cov(currentReturns())
-  })
-  
-  
-  # Moving Averages ------------------
-  
-  output$maPlots <- renderPlot({
-    symbols <- getSelectedSymbols(input)
-    start <- getStartDate(input)
-    end <- getEndDate(input)
-    
-    stockData <- get.closing.prices(symbols, start, end)
-    
-    par(mfrow=c(length(symbols),1))
-    
-    for (i in 1:length(symbols)) {
-      plot(cars, type="l", main="SMA")
-    }
-  })
-  
-  # Forecasting ----------------------
-  
-  # Optimization ---------------------
-  
-  output$optimizationFeasiblePlot <- renderPlot({
-    plot(cars)
-  })
-  
-  # -------------------------------
 })
